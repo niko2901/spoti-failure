41daf9c7693595ad4ff4d18c459402bbd2bf68d1
niko029 <168341022+niko2901@users.noreply.github.com>
2024-10-29 20:46:20 +0800

    unfinished pause play button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const form = document.getElementById('audioForm')
 const audio_input = document.getElementById('audio-in');
 const audio_player = document.getElementById('audio');
 
+var pause_stat = true;
+
 form.addEventListener('submit', function(event)
 {
     event.preventDefault();
@@ -16,24 +18,30 @@ form.addEventListener('submit', function(event)
     }
 })
 
-var pause_stat = true;
-
 function play_stat()
 {
     const play = document.getElementById('play');
     const pause = document.getElementById('pause');
 
+    try{
+        audio_player.play();
+    }catch(err){
+        console.log(err);
+    }
+
     if(pause_stat)
     {
         play.style.display = "none";
         pause.style.display = "block";
+        audio_player.play();
 
         pause_stat = false;
     }
     else {
         play.style.display = "block";
         pause.style.display = "none";
-        
+        audio_player.pause();
+
         pause_stat = true;
     }
 }
